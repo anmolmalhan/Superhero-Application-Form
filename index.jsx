921097f0469108c363bae0754fc1e81cd2c86525
Anmol Malhan <malhananmol1@gmail.com>
2025-12-08 22:22:14 +0530

    added some code in jsx file

--- index.jsx
+++ index.jsx
@@ -1,11 +1,21 @@
-const {useState} = React;
+const { useState } = React;
+
 export const SuperheroForm = () => {
-const [heroName, setHeroName] = useState('');
-const [realName, setRealName] = useState('');
-const [powers, setPowers] = useState([]);
-const [powerSource, setPowerSource] = useState('');
-  return(<div className="form-wrap">
-    <h2>Superhero Application Form</h2>
-    <p>Please complete all fields</p>
-  </div>)
+
+  const [heroName, setHeroName] = useState('');
+  const [realName, setRealName] = useState('');
+  const [powerSource, setPowerSource] = useState('');
+  const [powers, setPowers] = useState([]);
+
+  return (
+    <div className='form-wrap'>
+      <h2>Superhero Application Form</h2>
+      <p>Please complete all fields</p>
+      <form>
+        <div className='section'>
+          
+        </div>
+      </form>
+    </div>
+  )
 };
